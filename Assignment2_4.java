132adec076eb49c30bc7f40751561fc73bc0b566
SubrahmanyaGanapumane <subrahmanyagg@gmail.com>
2016-12-24 19:08:54 +0530

    Add files via upload

---
+++ Assignment2_4.java
@@ -0,0 +1,11 @@
+
+public class Assignment2_4 {
+	public static void main(String args[]){
+		int a=3,b=59,c=24;
+		
+		System.out.println("Given Elements are:"+a+ " "+b+" "+c);
+		System.out.print("MAximum element is: ");
+		System.out.println( (a>b?a:b)>c?(a>b?a:b):c);
+	}
+
+}
